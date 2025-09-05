91c6dbdf7dec5f1c19c571ea4ae46ddc332f5a7e
nazwazhrr <nazwaazahra883@gmail.com>
2025-09-05 09:13:56 +0700

    Jobsheet 2

---
+++ jobsheet02/jobsheet02/DemoPersegi.java
@@ -0,0 +1,11 @@
+package jobsheet02;
+
+public class DemoPersegi {
+    public static void main(String[] args) {
+        PersegiPanjang pp = new PersegiPanjang();
+        pp.panjang = 12;
+        pp.lebar = 8;
+
+        pp.displayInfo();
+    }
+}
--- jobsheet02/jobsheet02/MahasiswaDemo.java
+++ jobsheet02/jobsheet02/MahasiswaDemo.java
@@ -8,6 +8,20 @@ public class MahasiswaDemo {
         m1.alamat = "Nias, Sumatera Utara";
         m1.kelas = "2A";
 
+        Mahasiswa m2 = new Mahasiswa();
+        m2.nim = "033536";
+        m2.nama = "Nazwa";
+        m2.alamat = "Batu, Jawa Timur";
+        m2.kelas = "2D";
+
+        Mahasiswa m3 = new Mahasiswa();
+        m3.nim = "043637";
+        m3.nama = "Aisya";
+        m3.alamat = "Solo, Jawa";
+        m3.kelas = "2F";
+
         m1.displayBiodata();
+        m2.displayBiodata();
+        m3.displayBiodata();
     }
 }
