33571d34b521ef4ba7b1bbfcd6a4f434535fe1cb
nazwazhrr <nazwaazahra883@gmail.com>
2025-10-08 13:57:44 +0700

    Percobaan 5

--- jobsheet07/Dosen.java
+++ jobsheet07/Dosen.java
@@ -2,8 +2,11 @@ public class Dosen extends Pegawai{
     public String nidn;
     
     public Dosen() {
-        System.out.println(gaji);
         System.out.println("Objek dari class Dosen dibuat");
+    }
+
+    public String getInfo() {
+        return "NIDN    : " + this.nidn + "\n";
     }
 
     public String getAllInfo() {
