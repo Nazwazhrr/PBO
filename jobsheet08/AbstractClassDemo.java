6435d70c461065c5ff9c00ce1fc16fa08ff6b087
nazwazhrr <nazwaazahra883@gmail.com>
2025-11-01 20:20:44 +0700

    Percobaan 2 langkah 4

--- jobsheet08/AbstractClassDemo.java
+++ jobsheet08/AbstractClassDemo.java
@@ -3,11 +3,10 @@ public class AbstractClassDemo {
         Hewan hewan1 = new Hewan(10, "Rumput", "Savana");
         hewan1.cetakInfo();
         hewan1.bergerak();
-        hewan1.bernapas();
-
-        Lebah lebah1 = new Lebah("Ratu", 0.05, "Nektar", "Hutan");
-        lebah1.cetakInfo();
-        lebah1.bergerak();
-        lebah1.bernapas();
+        hewan1.bernapas();    
     }
 }
+// Lebah lebah1 = new Lebah("Ratu", 0.05, "Nektar", "Hutan");
+// lebah1.cetakInfo();
+// lebah1.bergerak();
+// lebah1.bernapas();
