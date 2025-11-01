18909229577a2ab7bd5e7608e3e079c4b7476a3d
nazwazhrr <nazwaazahra883@gmail.com>
2025-11-01 20:42:28 +0700

    Percobaan 2

--- jobsheet08/AbstractClassDemo.java
+++ jobsheet08/AbstractClassDemo.java
@@ -4,9 +4,10 @@ public class AbstractClassDemo {
         lebah1.cetakInfo();
         lebah1.bergerak();
         lebah1.bernapas();
+
+        Ular ular1 = new Ular(false, 2, "daging", "hutan");
+        ular1.cetakInfo();
+        ular1.bergerak();
+        ular1.bernapas();
     }
 }
-// Hewan hewan1 = new Hewan(10, "Rumput", "Savana");
-// hewan1.cetakInfo();
-// hewan1.bergerak();
-// hewan1.bernapas();    
